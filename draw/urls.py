c9b55dc8bcef55d629d2924f069c0477e98828f1
Brandon Lee <bblee@berkeley.edu>
2021-07-21 17:11:39 -0700

    finished all tasks

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
 
 ]
