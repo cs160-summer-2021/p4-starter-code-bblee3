3d0e9de30dacbfe9d26180135bff963d2e5fe689
Brandon Lee <bblee@berkeley.edu>
2021-07-22 17:17:03 -0700

    finished coding portion of 4.9

--- draw/urls.py
+++ draw/urls.py
@@ -7,5 +7,12 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('admin', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-
+    path('qOne', views.qOne, name='qOne'),
+    path('qTwo', views.qTwo, name='qTwo'),
+    path('qThree', views.qThree, name='qThree'),
+    path('qFour', views.qFour, name='qFour'),
+    path('qFive', views.qFive, name='qFive'),
+    path('bigScreen', views.bigScreen, name='bigScreen'),
+    path('bigScreenName', views.bigScreenName, name='bigScreenName'),
+    path('thankScreen', views.thankScreen, name='thankScreen'),
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,6 +4,30 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
+def qOne(request):
+    return render(request, 'draw/qOne.html')
+
+def qTwo(request):
+    return render(request, 'draw/qTwo.html')
+
+def qThree(request):
+    return render(request, 'draw/qThree.html')
+
+def qFour(request):
+    return render(request, 'draw/qFour.html')
+
+def qFive(request):
+    return render(request, 'draw/qFive.html')
+    
+def bigScreen(request):
+    return render(request, 'draw/bigScreen.html')
+
+def bigScreenName(request):
+    return render(request, 'draw/bigScreenName.html')
+
+def thankScreen(request):
+    return render(request, 'draw/thankScreen.html')
+
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name': room_name
